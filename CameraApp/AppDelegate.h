682442ed4f4b11baf4de56fcd5b3721f6caafd0e
annutech <annutech@annutechs-Mac-Pro-2.local>
2015-02-10 17:31:19 +0530

    Initial Commit

---
+++ CameraApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraApp
+//
+//  Created by annutech on 2/10/15.
+//  Copyright (c) 2015 annutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraApp
+//
+//  Created by annutech on 2/10/15.
+//  Copyright (c) 2015 annutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
